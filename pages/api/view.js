e7cda68cd6a1e3e92c61abc8f8d3fbd7fd9507a7
iremnaz <ic2268@nyu.edu>
2025-07-03 17:25:26 +0300

    cosole logs for view

--- pages/api/view.js
+++ pages/api/view.js
@@ -16,6 +16,9 @@ export default async function handler(req, res) {
     // Build full URL for public blob (adjust domain accordingly)
     const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'https://timeline-startad.vercel.app';
     const url = baseUrl + path;
+    console.log('path:', path);
+    console.log('url:', url);
+    console.log('VERCEL_URL:', process.env.VERCEL_URL);
 
     // Fetch the JSON content
     const response = await fetch(url);
@@ -23,9 +26,7 @@ export default async function handler(req, res) {
       return res.status(404).send('Timeline not found or expired.');
     }
     const data = await response.json();
-    console.log('path:', path);
-    console.log('url:', url);
-    console.log('VERCEL_URL:', process.env.VERCEL_URL);
+    
 
 
     res.setHeader('Content-Type', 'text/html');
